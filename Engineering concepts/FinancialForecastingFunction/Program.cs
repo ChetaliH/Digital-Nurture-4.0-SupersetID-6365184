241d71fa3003fca1158841be64de5d34367daed7
Chetali <chetalihariramani@gmail.com>
2025-06-22 19:52:59 +0530

    Financial forecasting function implementation project Hands-on

---
+++ Engineering concepts/FinancialForecastingFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\nRecursion is a method where a function calls itself to solve smaller instances of a problem,\nsimplifying complex tasks like factorials or financial predictions.");
+        double growthRate = 0.1;
+        int year = 5;
+        double initialValue = 100000;
+
+        Prediction predictor = new Prediction();
+        double result = predictor.CompoundGrowth(year, initialValue, growthRate);
+        Console.WriteLine($"\nThe forecast for {year} years is: " + result);
+
+        //Time Complexity Analysis=>
+        Console.WriteLine("\nThe CompoundGrowth function runs O(N) times, where N is the number of years.");
+        Console.WriteLine("\nThis recursion is optimal only because it depends on one previous result.");
+        Console.WriteLine("\nFor over-lapping sub-problems, we must use memoization or choose iterative approach.\n");
+
+
+    }
+}
+
+class Prediction
+{
+    public double CompoundGrowth(int year, double initialValue, double growthRate)
+    {
+        if (year == 0)
+        {
+            return initialValue;
+        }
+        return CompoundGrowth(year - 1, initialValue, growthRate) * (1 + growthRate);
+    }
+}
